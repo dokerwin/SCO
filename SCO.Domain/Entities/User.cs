856ab3078784042fb7e357be671737042ac1de4e
Zakhar Kulbachenko <nik.name.doker@gmail.com>
2022-10-19 22:48:37 +0200

    Add User Entity

---
+++ SCO.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+namespace SCO.Domain.Entities;
+public class User
+{
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string Firstname { get; set; }
+    public string LastName { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public string PasswordHash { get; set; }
+    public int RoleId { get; set; }
+    public virtual Role Role { get; set; }
+}
