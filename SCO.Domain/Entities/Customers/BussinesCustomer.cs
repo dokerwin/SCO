456de79fa68da721d4b6c5656248106f1fcaa907
Zakhar Kulbachenko <nik.name.doker@gmail.com>
2022-11-19 14:56:57 +0100

    Domain add BussinesCustomer entity

---
+++ SCO.Domain/Entities/Customers/BussinesCustomer.cs
@@ -0,0 +1,11 @@
+﻿using SCO.Domain.Entities.Base;
+using SCO.Domain.Entities.Company;
+
+namespace SCO.Domain.Entities.Customers;
+public class BussinesCustomer : EntityBase<Guid>
+{
+    public Guid CustomerId { get; set; }
+    public Customer? Customer { get; set; }
+    public Guid CompanyId { get; set; }
+    public CustomerCompany? CustomerCompany { get; set; }
+}
