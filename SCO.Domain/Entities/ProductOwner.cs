ba2160c6889f6016e86074d346c4baf8625e5253
Zakhar Kulbachenko <nik.name.doker@gmail.com>
2022-11-19 14:49:36 +0100

    Domain add ProductOwner entity

---
+++ SCO.Domain/Entities/ProductOwner.cs
@@ -0,0 +1,10 @@
+﻿using SCO.Domain.Entities.Base;
+
+namespace SCO.Domain.Entities;
+
+public class ProductOwner : EntityBase<Guid>
+{
+    public string Name { get; set; }
+    public int Type { get; set; }
+}
+
