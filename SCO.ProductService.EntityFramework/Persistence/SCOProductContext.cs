cc7c5c4f97d3b70c4b870db558aa5a54b3664c8a
Zakhar Kulbachenko <nik.name.doker@gmail.com>
2022-12-13 23:05:02 +0100

    Add SCO.ProductService.EntityFramework first state

---
+++ SCO.ProductService.EntityFramework/Persistence/SCOProductContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using SCO.ProductService.Domain.Entities;
+
+
+namespace SCO.ProductService.EntityFramework.Persistence;
+
+public class SCOProductContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+    public SCOProductContext(DbContextOptions<SCOProductContext> dbContextOptions, IConfiguration configuration)
+           : base()
+    {
+        _configuration = configuration;
+    }
+    public virtual DbSet<Category> Categories { get; set; }
+    public virtual DbSet<ProductOwner> ProductOwners { get; set; }
+    public virtual DbSet<Product> Products { get; set; }
+    public virtual DbSet<Tax> Taxes { get; set; }
+    public virtual DbSet<Vat> Vats { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(SCOProductContext).Assembly);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_configuration.GetConnectionString("SCO_ProductService_ConnectionString"));
+    }
+}
+
