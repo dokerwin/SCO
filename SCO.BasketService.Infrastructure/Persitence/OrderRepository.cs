54fdd120a02445d5058f26cb73db94aa2ac4c4aa
Zakhar Kulbachenko <nik.name.doker@gmail.com>
2022-12-13 22:46:22 +0100

    Add SCO.BasketService.Infrastructure first state

---
+++ SCO.BasketService.Infrastructure/Persitence/OrderRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SCO.BasketService.Application.Common.Interfaces.Persistance;
+using SCO.BasketService.Domain.Entities;
+using SCO.BasketService.EntityFramework.Persistence;
+
+namespace SCO.BasketService.Infrastructure.Persitence;
+
+public class OrderRepository : Repository<Order>, IOrderRepository
+{
+    public OrderRepository(SCOBasketServiceContext context, ILogger logger) : base(context, logger)
+    {
+    }
+
+    public async Task<Order> GetActualOrder()
+    {
+        try
+        {
+            var actualShift = await _dbSet.LastOrDefaultAsync();
+            if (actualShift is not null)
+                return actualShift;
+
+            return  new Order();
+
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "{Repo} Upsert function error", typeof(OrderRepository));
+            return new Order();
+        }
+    }
+
+    public override async Task<bool> Upsert(Order entity)
+    {
+        try
+        {
+            var existingRole = await _dbSet.Where(x => x.Id == entity.Id)
+                                              .FirstOrDefaultAsync();
+            if (existingRole == null)
+                return await Add(entity);
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "{Repo} Upsert function error", typeof(OrderRepository));
+            return false;
+        }
+    }
+}
+
---
+++ SCO.BasketService.Infrastructure/Persitence/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+using SCO.BasketService.Application.Common.Interfaces.Persistance;
+using SCO.BasketService.EntityFramework.Persistence;
+
+
+namespace SCO.BaskerService.Infrastructure.Persitence;
+
+public class UnitOfWork : IUnitOfWork, IDisposable
+{
+    private readonly SCOBasketServiceContext _context;
+    private readonly ILogger _logger;
+
+   
+    public IOrderRepository Orders { get; private set; }
+   
+    public UnitOfWork(SCOBasketServiceContext context,
+        ILoggerFactory loggerFactory, IOrderRepository orderRepository
+       
+        )
+    {
+        _context = context;
+        _logger = loggerFactory.CreateLogger("logs");
+
+        Orders = orderRepository;
+        
+    }
+
+    public async Task CompleteAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+
+}
