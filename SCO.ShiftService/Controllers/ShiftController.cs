18618624a38148684a3a9ea0f5f7269bbdd5720c
Zakhar Kulbachenko <nik.name.doker@gmail.com>
2023-01-15 12:25:13 +0100

    Add ShiftService

---
+++ SCO.ShiftService/Controllers/ShiftController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SCO.ShiftService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShiftController : ControllerBase
+    {
+        // GET: api/<ShiftController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ShiftController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ShiftController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ShiftController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ShiftController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
